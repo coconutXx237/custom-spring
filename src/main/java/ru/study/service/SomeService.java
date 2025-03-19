21db3e9ace16c62176723f4b18cccd02d3b756c1
oklimkin <oklimkin@neoflex.ru>
2025-03-19 20:11:24 +0300

    bean injecting added

---
+++ src/main/java/ru/study/service/SomeService.java
@@ -0,0 +1,8 @@
+package ru.study.service;
+
+public class SomeService {
+
+    public SomeService() {
+        System.out.println("SomeService Bean created");
+    }
+}
---
+++ src/main/java/ru/study/util/StringConstants.java
@@ -0,0 +1,5 @@
+package ru.study.util;
+
+public class StringConstants {
+    public static final String SCANNING_PATH = "ru.study.model";
+}
