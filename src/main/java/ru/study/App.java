8f5b502c5670149b9c43e73ea46b2bfa4225260e
oklimkin <oklimkin@neoflex.ru>
2025-03-20 22:17:39 +0300

    createUser amended to getBean

--- src/main/java/ru/study/App.java
+++ src/main/java/ru/study/App.java
@@ -1,14 +1,15 @@
 package ru.study;
 
 import ru.study.model.User;
-import ru.study.util.UserFactory;
+import ru.study.util.ObjectFactory;
 
 public class App
 {
     public static void main( String[] args ) {
 
-        UserFactory userFactory = new UserFactory();
-        User user = userFactory.createUser();
+        ObjectFactory objectFactory = new ObjectFactory();
+        User user = objectFactory.getBean(User.class);
         System.out.println(user);
+
     }
 }
--- src/main/java/ru/study/util/ObjectFactory.java
+++ src/main/java/ru/study/util/ObjectFactory.java
@@ -5,7 +5,6 @@ import org.reflections.scanners.Scanners;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 import ru.study.annotation.Bean;
-import ru.study.model.User;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -16,12 +15,12 @@ import java.util.Set;
 import static ru.study.util.StringConstants.SCANNING_PATH;
 
 
-public class UserFactory {
+public class ObjectFactory {
 
     private static final Map<Class<?>, Object> beanContainer = new HashMap<>();
     private final Reflections reflections;
 
-    public UserFactory() {
+    public ObjectFactory() {
         reflections = new Reflections(
                 new ConfigurationBuilder()
                         .forPackages(SCANNING_PATH)
@@ -30,21 +29,25 @@ public class UserFactory {
         );
     }
 
-    public User createUser() {
-        User user = new User();
-        createBeans();
-        injectBeans(user);
-        return user;
+    public <T> T getBean(Class<T> clazz) {
+        try {
+            var obj = clazz.getDeclaredConstructor().newInstance();
+            createBeans();
+            injectBeans(obj);
+            return obj;
+        } catch (InstantiationException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private void injectBeans(User user) {
-        Field[] fields = user.getClass().getDeclaredFields();
+    private void injectBeans(Object obj) {
+        Field[] fields = obj.getClass().getDeclaredFields();
         for (Field field : fields) {
             if (field.isAnnotationPresent(Bean.class)) {
                 field.setAccessible(true);
                 var bean = beanContainer.get(field.getType());
                 try {
-                    field.set(user, bean);
+                    field.set(obj, bean);
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException(e);
                 }
@@ -65,7 +68,6 @@ public class UserFactory {
             }
         }
     }
-
 }
 
 
