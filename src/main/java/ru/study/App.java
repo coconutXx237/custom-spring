59e3639f6406cf54d965581f51f71eabf639c60e
oklimkin <oklimkin@neoflex.ru>
2025-03-28 16:56:03 +0300

    upd

--- src/main/java/ru/study/App.java
+++ src/main/java/ru/study/App.java
@@ -12,25 +12,14 @@ import java.time.Instant;
 public class App
 {
     public static void main( String[] args ) {
+
         ObjectFactory objectFactory = new ObjectFactory();
+
         Person person = objectFactory.getBean(Person.class);
         System.out.println(person);
 
-/*        Enhancer enhancer = new Enhancer();
-        enhancer.setSuperclass(Person.class);
-        enhancer.setCallback((MethodInterceptor) (obj, method, argss, proxy) -> {
-         //   if (method.getDeclaringClass() != Object.class && method.getReturnType() == String.class) {
-            //    return "Hello Tom!";
-          //  } else {
-                System.out.printf("Метод %S у объекта %s вызвался во время %s", method.getName(), proxy.hashCode(), Instant.now());
-                return proxy.invokeSuper(obj, argss);
-            //}
-        });
-
-        Person proxyPerson = (Person) enhancer.create();*/
-
-        // System.out.println("Age is: " + proxyPerson.getAge());
-        //person.sayAge();
+        User user = objectFactory.getBean(User.class);
+        System.out.println(user);
 
     }
 }
